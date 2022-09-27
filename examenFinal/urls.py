ddb445156c097ff222f6cb1af868dfa5a330ee3b
waldir merjildo <muwaldir@gmail.com>
2022-09-27 14:20:44 -0500

    avance de la venta modal y el codigo

--- examenFinal/urls.py
+++ examenFinal/urls.py
@@ -6,4 +6,9 @@ app_name = 'examenFinal'
 urlpatterns = [
     path('',views.index,name='index'),
     path('dashboard',views.dashboard,name='dashboard'),
+    path('obtener_info_tarea',views.obtener_info_tarea, name='tarea_info'),
+    path('nuevaTarea', views.nuevaTarea, name='nuevaTarea'),
+    path('eliminarTarea',views.eliminarTarea, name='eliminarTarea'),
+    path('editarTarea', views.editarTarea, name='editarTarea'),
+    path('actualizarTarea/<str:tarea_id>', views.actualizarTarea, name="actualizarTarea"),
 ]
--- examenFinal/views.py
+++ examenFinal/views.py
@@ -1,7 +1,11 @@
+import json
 from django.shortcuts import render
 from django.urls import reverse
+
+
 from .models import tareasExamen, usuariosFinal
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect, JsonResponse
+
 
 # Create your views here.
 def index(request):
@@ -28,3 +32,65 @@ def dashboard(request):
     return render(request,'examenFinal/dashboard.html',{
         'tareas_totales':tareasExamen.objects.all().order_by('id')
     })
+
+def obtener_info_tarea(request):
+   
+    id_tarea = str(request.GET.get('tarea_id'))
+    id_t =id_tarea.split('detalle')
+    tarea = tareasExamen.objects.filter(id=id_t[1])
+    tarea_info = []
+    for info in tarea:
+        tarea_info.append([info.fechaCreacion,info.fechaEntrega,info.descripcion,info.estadoTarea])
+
+    return JsonResponse({
+        'tareas':tarea_info
+    })
+
+def nuevaTarea(request):
+    informacion = json.load(request)
+    f_creacion= informacion.get('f_creacion')
+    f_entrega = informacion.get('f_entrega')
+    descripcion = informacion.get('descripcion')
+    estado= informacion.get('estado')
+    tareasExamen(fechaCreacion = f_creacion, fechaEntrega=f_entrega, descripcion=descripcion,estadoTarea=estado).save()   
+    return JsonResponse({
+        'info':'tarea aceptada'
+    })
+  
+def eliminarTarea(request):
+    id_tarea = str(request.GET.get('ta_id'))
+    id_t =id_tarea.split('eliminar')
+    tarea = tareasExamen.objects.filter(id=id_t[1])
+    tarea.delete()
+    return JsonResponse({
+        'info':'tarea borrada'
+    })
+
+def editarTarea(request):
+    id_tarea = str(request.GET.get('tarea_id'))
+    id_t =id_tarea.split('editar')
+    tarea = tareasExamen.objects.filter(id=id_t[1])
+    tarea_info = []
+    for info in tarea:
+        tarea_info.append([info.fechaCreacion,info.fechaEntrega,info.descripcion,info.estadoTarea])
+
+    return JsonResponse({
+        'tareas':tarea_info
+    })
+
+def actualizarTarea(request, tarea_id):
+    tarea_ac = tareasExamen.objects.get(id=tarea_id)
+    print(tarea_ac)
+    if request.method == 'POST':
+        t_creacion = request.POST.get('fecha_creacion')
+        t_entrega= request.POST.get('fecha_entrega')
+        descripcion = request.POST.get('descripcion')
+        estado = request.POST.get('estado')
+        tarea_ac.fechaCreacion = t_creacion
+        tarea_ac.fechaEntrega = t_entrega
+        tarea_ac.descripcion = descripcion
+        tarea_ac.estadoTarea = estado
+        tarea_ac.save()
+        return HttpResponseRedirect(reverse('examenFinal:dashboard'))
+
+    return HttpResponseRedirect(reverse('examenFinal:dashboard'))
